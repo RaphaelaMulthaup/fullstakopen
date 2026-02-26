1b6b159a09826bb45e2585e4d7bf01a1480ee008
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-02-26 09:07:56 +0100

    feat: 1.4: Course Information step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,25 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -31,17 +29,18 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  const { parts } = props;
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   );
 };
 
 const Part = (props) => {
-  const {name, exercises} = props.part;
+  const { name, exercises } = props.part;
   return (
     <p>
       {name} {exercises}
@@ -50,10 +49,11 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
+  const { parts } = props;
   return (
     <p>
       Number of exercises
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
