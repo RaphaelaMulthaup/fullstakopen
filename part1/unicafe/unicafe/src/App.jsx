b16cb233ff46cfe9d67998dc09e627b5853ed7d7
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-03-02 16:21:32 +0100

    feat: 1.6: unicafe step 1

---
+++ part1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Feedback
+        setGood={setGood}
+        setNeutral={setNeutral}
+        setBad={setBad}
+      ></Feedback>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      ></Statistics>
+    </div>
+  );
+};
+
+const Feedback = ({ setGood, setNeutral, setBad }) => {
+  return (
+    <>
+      {" "}
+      <h2>Give feedback</h2>
+      <div>
+        <Button
+          onClick={() => setGood((prev) => prev + 1)}
+          name={"good"}
+        ></Button>
+        <Button
+          onClick={() => setNeutral((prev) => prev + 1)}
+          name={"neutral"}
+        ></Button>
+        <Button
+          onClick={() => setBad((prev) => prev + 1)}
+          name={"bad"}
+        ></Button>
+      </div>
+    </>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      {" "}
+      <h2>Statistics</h2>
+      <div>
+        <Display name={"good "} amount={good}></Display>
+        <Display name={"neutral "} amount={neutral}></Display>
+        <Display name={"bad "} amount={bad}></Display>
+      </div>
+    </>
+  );
+};
+
+const Button = ({ onClick, name }) => {
+  return <button onClick={onClick}>{name}</button>;
+};
+
+const Display = ({ name, amount }) => {
+  return (
+    <div>
+      {name}
+      {amount}
+    </div>
+  );
+};
+
+export default App;
