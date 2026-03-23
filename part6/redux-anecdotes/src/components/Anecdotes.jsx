ce2671be02392253e1d2d1e711c0156de4c8ed6b
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2026-03-23 08:20:20 +0100

    feat: 6.3: Anecdotes, step 1

---
+++ part6/redux-anecdotes/src/components/Anecdotes.jsx
@@ -0,0 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
+import { vote } from "../reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  );
+};
+
+const Anecdotes = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  return (
+    <>
+      {anecdotes.map((a) => (
+        <Anecdote
+          key={a.id}
+          anecdote={a}
+          handleClick={() => dispatch(vote(a.id))}
+        ></Anecdote>
+      ))}
+    </>
+  );
+};
+
+export default Anecdotes;
